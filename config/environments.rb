d75cbe0e5e4ddac1177b4990f3172f91a0867f0f
Chandler Smith & Alex Birdsall <pair+ChandlerHCSmith.ambirdsall@devbootcamp.com>
2014-08-26 10:46:22 -0700

    Adds some environment configuration for app and database

---
+++ config/environments.rb
@@ -0,0 +1,25 @@
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+#======^environment config^===========================
+
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
+
+DB_NAME = db.path[1..-1]
+
+# Note:
+#   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
+#   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
+#   to :development
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => DB_NAME,
+  :encoding => 'utf8'
+)
+#======^database config^===========================
