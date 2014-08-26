faea8783bc8cd680fdc5ab086191933b0b828e6d
Tommy Dugger & "There once was a man from Nantucket <pair+cerinman.yohanyi@devbootcamp.com>
2014-08-26 10:50:08 -0700

    Adds alogrithm logic for getting up and down votes for a user

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -1,1 +1,21 @@
 require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+get '/users/:id/up_votes' do
+  #Get records from DB
+  votes = Vote.where(user_id: params[:id])
+  #pass those records to our gem
+  MusicMatcher.up_votes(votes)
+  #return the results
+end
+
+get '/users/:id/down_votes' do
+  #Get records from DB
+  votes = Vote.where(user_id: params[:id])
+  #pass those records to our gem
+  MusicMatcher.down_votes(votes)
+  #return the results
+end
--- lib/music_matcher.rb
+++ lib/music_matcher.rb
@@ -1,3 +1,21 @@
 module MusicMatcher
+  def self.up_votes(votes)
+    genre_up_votes = Hash.new(0)
+
+    votes.each do |vote|
+      if vote.up_votes
+        genre_up_votes[vote.genre_name] += 1
+      end
+    end
+  end
+
+  def self.down_votes(votes)
+    genre_down_votes = Hash.new(0)
+    votes.each do |vote|
+      if vote.up_votes
+        genre_down_votes[vote.genre_name] += 1
+      end
+    end
+  end
 
 end
