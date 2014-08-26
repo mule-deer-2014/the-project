a3cd0c2a2d15cd5274b2d337ae55a95359366de4
Matt <matt16749@gmail.com>
2014-08-26 00:31:45 -0700

    adds test template for api/users

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,45 +1,22 @@
-# Set up gems listed in the Gemfile.
-# See: http://gembundler.com/bundler_setup.html
-#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require_relative 'spec_helper.rb'
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+describe 'controllers.rb' do 
+  describe '/' do 
+    it 'exists' do 
+      get '/'
+      expect(last_response.status).to eq(302)
+    end
+  end
 
-# Require gems we care about
-require 'rubygems'
+  describe '/api' do 
+    # How do I test a conditional?
+  end
 
-require 'uri'
-require 'pathname'
+  describe '/api/users/:id' do 
+    # This route takes a params. What is the setup?
+  end
 
-require 'pg'
-require 'active_record'
-require 'logger'
-
-require 'sinatra'
-require "sinatra/reloader" if development?
-
-require 'erb'
-
-# Some helper constants for path-centric logic
-APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-
-APP_NAME = APP_ROOT.basename.to_s
-
-configure do
-  # By default, Sinatra assumes that the root is the file that calls the configure block.
-  # Since this is not the case for us, we set it manually.
-  set :root, APP_ROOT.to_path
-  # See: http://www.sinatrarb.com/faq.html#sessions
-  enable :sessions
-  set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
-
-  # Set the views to
-  set :views, File.join(Sinatra::Application.root, "app", "views")
+  describe '/api/users' do
+    #This route returns a json object. How do I test that?
+  end
 end
-
-# Set up the controllers and helpers
-Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
-Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
-
-# Set up the database and models
-require APP_ROOT.join('config', 'database')
