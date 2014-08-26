6d65fdf774e2bf68f6b9626b90c7fe059ba3dfe8
Matt <matt16749@gmail.com>
2014-08-26 01:07:39 -0700

    adds restful routes for songs, genres, and votes models

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -22,7 +22,7 @@ get '/api' do
   ap 'Hello user!', options = {:color => {:string => :pale}}
   ap 'Welcome to our API!',  options = {:color => {:string => :greenish}}
   ap 'Please enter a command...', options = {:color => {:string => :purple}}
-  ap "select_user, all_users",  options = {:color => {:string => :redish}}
+  ap ["select_user", "all_users", "select_song", "all_songs", "select_genre", "all_genres", "select_vote", "all_votes"],  options = {:color => {:string => :redish}}
   admin_input = gets.chomp
   case admin_input
     when 'select_user'
@@ -31,26 +31,93 @@ get '/api' do
       redirect "/api/users/#{user_id}"
     when 'all_users'
       redirect '/api/users'
+    when 'select_song'
+      ap 'Please enter song_id' ,  options = {:color => {:string => :blue}}
+      song_id = gets.chomp
+      redirect "/api/songs/#{song_id}"
+    when 'all_songs'
+      redirect '/api/songs'
+    when 'select_genre'
+      ap 'Please enter genre_id' ,  options = {:color => {:string => :blue}}
+      genre_id = gets.chomp
+      redirect "/api/genres/#{genre_id}"
+    when 'all_genres'
+      redirect '/api/genres'
+    when 'select_vote'
+      ap 'Please enter vote_id' ,  options = {:color => {:string => :blue}}
+      vote_id = gets.chomp
+      redirect "/api/votes/#{vote_id}"
+    when 'all_votes'
+      redirect '/api/votes'
   end
   # admin_input <= This is the recursive? call from line 4.
 end
 
-get '/api/users/:id' do
-  user_id = params[:id]
-  user = User.find(user_id)
-  ap "Id: #{user.id}, Name: #{user.name}" 
-  # Change above to a json object
-end
-
-get '/api/users' do 
-  users = User.all 
-  users.each do |user|
-      ap "Id: #{user.id}, Name: #{user.name}"
-      # Change above to a json object
+# USERS
+  get '/api/users/:id' do
+    user_id = params[:id]
+    user = User.find(user_id)
+    ap "Id: #{user.id}, Name: #{user.name}" 
+    # Change above to a json object
   end
-end
 
+  get '/api/users' do 
+    users = User.all 
+    users.each do |user|
+        ap "Id: #{user.id}, Name: #{user.name}"
+        # Change above to a json object'
+    end
+  end
 
+# SONGS
+  get '/api/songs/:id' do
+    song_id = params[:id]
+    song = Song.find(song_id)
+    ap "Id: #{song.id}, Title: #{song.title}" 
+    # Output Genre via genre_id as well.
+    # Change above to a json object
+  end 
+
+  get '/api/songs' do 
+    songs = Song.all 
+    songs.each do |song|
+      ap "Id: #{song.id}, Title: #{song.title}"
+      # Output Genre via genre_id as well.    
+      # Change above to a json object
+    end
+  end  
+
+# GENRES
+  get '/api/genres/:id' do
+    genre_id = params[:id]
+    genre = Genre.find(genre_id)
+    ap "Id: #{genre.id}, Name: #{genre.name}" 
+    # Change above to a json object
+  end
+
+  get '/api/genres' do 
+    genres = Genre.all 
+    genres.each do |genre|
+      ap "Id: #{genre.id}, Name: #{genre.name}"
+      # Change above to a json object
+    end
+  end  
+
+# VOTES
+  get '/api/votes/:id' do
+    vote_id = params[:id]
+    vote = Vote.find(vote_id)
+    ap "Id: #{vote.id}, User Id: #{vote.user_id},  Song Id: #{vote.song_id}, Votes: #{vote.votes}" 
+    # Change above to a json object
+  end
+
+  get '/api/votes' do 
+    users = Vote.all 
+    users.each do |vote|
+      ap "Id: #{vote.id}, User Id: #{vote.user_id},  Song Id: #{vote.song_id}, Votes: #{vote.votes}" 
+      # Change above to a json object
+    end
+  end
 
 
 #EXAMPLE ROUTES FOR NON-API
