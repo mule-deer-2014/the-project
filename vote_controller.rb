cd0d0f2a8739da156633e1eb28c5487d15123245
Chandler Smith & Alex Birdsall <pair+ChandlerHCSmith.ambirdsall@devbootcamp.com>
2014-08-26 12:32:01 -0700

    Slaps up some routing logic

--- vote_controller.rb
+++ vote_controller.rb
@@ -6,3 +6,20 @@ require './config/environments'
 get '/' do
   "Yay! My bike is fixed!"
 end
+
+
+get '/song' do
+  if sessions[:user_id]
+    # serve them a song
+end
+
+# create a new vote
+post '/users/:id/votes' do
+  if params[:vote_type] == "upvote"
+    is_upvote = true
+  else
+    is_upvote = false
+  end
+
+  Vote.create({user_id = params[:id], is_upvote = some_var, genres = params[:genres]})
+end
