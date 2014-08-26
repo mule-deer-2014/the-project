a165c4f26daa2306d22aa67936d3428cba877bae
Cassidy Clawson & Danielle Sublett <pair+cassidyclawson.daniellesublett@devbootcamp.com>
2014-08-25 18:16:24 -0700

    first commit (skeleton)

---
+++ da_project/source/app/controllers/controller.rb
@@ -0,0 +1,34 @@
+require 'pg'
+require 'active_record'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+get "/" do
+  "hello world!"
+end
+
+
+
+
+
+
+
+
+
+
+
+# Soundcloud GET - retreieve 10 seonds of a song for the user view.
+
+
+# TERMINAL
+
+
+
+
+
+
+
+
---
+++ da_project/source/config.ru
@@ -0,0 +1,2 @@
+require "./app/controllers/controller"
+run Sinatra::Application
