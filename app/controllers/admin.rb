09061678a282bbcb1e564dda678bd4e4be37ae37
Tommy Dugger & "There once was a man from Nantucket <pair+cerinman.yohanyi@devbootcamp.com>
2014-08-26 10:51:41 -0700

    results from algorithm now returned from route as json

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -8,14 +8,12 @@ get '/users/:id/up_votes' do
   #Get records from DB
   votes = Vote.where(user_id: params[:id])
   #pass those records to our gem
-  MusicMatcher.up_votes(votes)
-  #return the results
+  MusicMatcher.up_votes(votes).to_json
 end
 
 get '/users/:id/down_votes' do
   #Get records from DB
   votes = Vote.where(user_id: params[:id])
   #pass those records to our gem
-  MusicMatcher.down_votes(votes)
-  #return the results
+  MusicMatcher.down_votes(votes).to_json
 end
