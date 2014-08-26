491eddb6ca9514abf93b3f85d1473812edea7fdf
Matt <matt16749@gmail.com>
2014-08-25 20:57:02 -0700

    makes controllers more RESTful

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,13 +1,26 @@
 require 'sinatra'
 
 get '/' do 
+  redirect '/api'
+end
+
+get '/api' do 
   puts 'Hello user!'
   puts 'Welcome to our API!'
   puts 'Please enter a command...'
-  @input = gets.chomp
-  redirect "/command/#{@input}"
+  admin_input = gets.chomp
+  case admin_input
+    when 'new'
+      redirect '/api/new'
+    when 'edit'
+      redirect '/api/edit'
+  end
 end
 
-get '/command/:input' do
-  puts "I'm in command! #{params[:input]}"
+get '/api/new' do 
+  puts 'this is new'
+end
+
+get '/api/edit' do
+  puts 'this is edit'
 end
