d91c864e78e18a8c767f137cdd6b5bbef21b23d8
Tommy Dugger & "There once was a man from Nantucket <pair+cerinman.yohanyi@devbootcamp.com>
2014-08-26 11:38:24 -0700

    Fixes error in migration file, minor changes in seeds.rb file

--- db/migrate/20140825173550_create_votes.rb
+++ db/migrate/20140825173550_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :user_id, :song_id, :genre_name
+      t.integer :user_id, :song_id
+      t.string :genre_name
       t.boolean :up_vote, :down_vote
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
-genders = ['male', 'female']
-genres = ['rock', 'punk', 'pop', 'soul', '80s']
+genders = ["male", "female"]
+genre_samples = ["rock", "punk", "pop", "soul", "80s"]
 
 5.times do
   User.create(name: Faker::Name.name, gender: genders.sample)
@@ -11,6 +11,6 @@ end
   song = Song.create(title: Faker::Company.name, song_id: x+1)
 
   5.times do
-    song.votes.create(user_id: rand(1..5), genre_name: genres.sample, up_vote: true, down_vote: nil)
+    song.votes.create(user_id: rand(1..5), genre_name: genre_samples.sample, up_vote: true, down_vote: nil)
   end
 end
