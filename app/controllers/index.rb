542e68378e36f86864e2a1c6159fd628be1a1999
Antonio Dangond & Ori <pair+antoniodangond.opleban@devbootcamp.com>
2014-08-25 17:30:56 -0700

    adds get 'users/:id' route and spec tests

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,9 @@
 get '/users' do
+  content_type :json
+  User.all.to_json
+end
 
+get '/users/:id' do
+  content_type :json
+  User.find(params[:id]).to_json
 end
--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,19 +1,37 @@
 require_relative '../spec_helper'
-
-describe "GET /users" do
+describe "User controller" do
   before do
     User.destroy_all
   end
 
-  it "responds successfully" do
-    get '/users'
-    expect(last_response.status).to eq(200)
-  end
-  # it "returns all users" do
-  #   user1 = User.create()
-  #   user2 = User.create()
+  describe "GET /users" do
 
-  #   get '/users'
-  #   expect(res)
-  # end
+    it "responds successfully" do
+      get '/users'
+      expect(last_response.status).to eq(200)
+    end
+    it "returns all users" do
+      user1 = User.create()
+      user2 = User.create()
+      get '/users'
+      expect(last_response.body).to eq(User.all.to_json)
+    end
+  end
+
+  describe "GET /users/:id" do
+    before do
+      @user = User.create
+    end
+
+    it "responds successfully" do
+      get "/users/#{@user.id}"
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the user" do
+      get "/users/#{@user.id}"
+      @user.to_json
+    end
+
+  end
 end
