ce7826b94ee60becae4b2936f6c4f0240be12bf2
kbasma <katia.bazzi@gmail.com>
2014-08-25 21:10:36 -0700

    changed the name of downvote genre join table in the database to make it work

--- scinder/db/migrate/20140825173746_create_genres_downvotes.rb
+++
@@ -1,8 +0,0 @@
-class CreateGenresDownvotes < ActiveRecord::Migration
-  def change
-    create_table :genres_downvotes do |t|
-      t.integer :downvote_id
-      t.integer :genre_id
-    end
-  end
-end
