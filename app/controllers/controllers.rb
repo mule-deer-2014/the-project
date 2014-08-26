148aa6b9e003a367d7bef5662b189697fcba3012
Matt <matt16749@gmail.com>
2014-08-25 21:54:00 -0700

    adds api restful routes

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -8,19 +8,49 @@ get '/api' do
   puts 'Hello user!'
   puts 'Welcome to our API!'
   puts 'Please enter a command...'
+  puts 'select_user, all_users'
   admin_input = gets.chomp
   case admin_input
-    when 'new'
-      redirect '/api/new'
-    when 'edit'
-      redirect '/api/edit'
+    when 'select_user'
+      puts 'Please enter user_id'
+      user_id = gets.chomp
+      redirect "/api/users/#{user_id}"
+    when 'all_users'
+      redirect '/api/users'
   end
 end
 
-get '/api/new' do 
-  puts 'this is new'
+get '/api/users/:id' do
+  user_id = params[:id]
+  user = User.find(user_id)
+  puts "Id: #{user.id}, Name: #{user.name}"
+  # Change above to a json object
 end
 
-get '/api/edit' do
-  puts 'this is edit'
+get '/api/users' do 
+  users = User.all 
+  users.each do |user|
+    puts "Id: #{user.id}, Name: #{user.name}"
+  end
 end
+
+# ROUTES FOR NON-API
+# get '/users/new' do 
+#   puts 'Please enter a new name'
+#   user_input = gets.chomp
+#   User.create(name: user_input)
+# end
+
+# get '/users' do
+#   puts 'this is displays all users'
+#   users = User.all
+#   users.each do |user|
+#     puts "Id: #{user.id}, Name: #{user.name}"
+#   end
+# end
+
+# get '/users/:id' do
+#   user_id = params[:id] 
+#   user = User.find(user_id)
+#   puts "Id: #{user.id}, Name: #{user.name}"
+# end
